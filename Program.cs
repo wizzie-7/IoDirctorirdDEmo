7b8c3b3caec49050d50971b41f1fd828db9dae74
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-08-30 19:11:05 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IoDirctorirdDEmo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
